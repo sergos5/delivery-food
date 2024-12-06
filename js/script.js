9705c2ba9c5d38ea44794d82f2b27fa0c7a1b6f2
Sergey Opekunov <sergos5@yandex.ru>
2024-12-06 22:55:55 +0300

    добавиь скрипт модального окна

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 'use strict'
 
-console.log('hello, world');
+const modal = document.querySelector('.cart-modal__overlay');
+const cartBtn = document.querySelector('#cart-button');
+const closeBtn = document.querySelector('.cart-modal__header--close')
 
+const openModal = () => {
+     modal.classList.add('open') 
+}
+
+const closeModal = () => {
+    modal.classList.remove('open') 
+}
+
+cartBtn.addEventListener('click', ()=> {
+    openModal()
+})
+
+closeBtn.addEventListener('click', ()=> {
+    closeModal()
+})
 
 
