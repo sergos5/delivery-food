62669729c91b40c0ffe263efa55e01f44b675d99
Sergey Opekunov <sergos5@yandex.ru>
2024-12-07 09:35:02 +0300

    доработал модальное окно

--- js/script.js
+++ js/script.js
@@ -1,23 +1,31 @@
 'use strict'
 
-const modal = document.querySelector('.cart-modal__overlay');
-const cartBtn = document.querySelector('#cart-button');
-const closeBtn = document.querySelector('.cart-modal__header--close')
+const modalFunc = () => {
+    const modal = document.querySelector('.cart-modal__overlay');
+    const cartBtn = document.querySelector('#cart-button');
 
-const openModal = () => {
-     modal.classList.add('open') 
+
+    const openModal = () => {
+        modal.classList.add('open') 
+    }
+
+    const closeModal = () => {
+        modal.classList.remove('open') 
+    }
+
+    cartBtn.addEventListener('click', ()=> {
+        openModal()
+    })
+
+    modal.addEventListener('click', (e)=> {
+        if (e.target.classList.contains('cart-modal__overlay') || 
+            e.target.closest('.cart-modal__header--close')) {
+            closeModal()   
+        }
+    
+    })
 }
 
-const closeModal = () => {
-    modal.classList.remove('open') 
-}
 
-cartBtn.addEventListener('click', ()=> {
-    openModal()
-})
-
-closeBtn.addEventListener('click', ()=> {
-    closeModal()
-})
-
+modalFunc()
 
